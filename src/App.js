2e2d133b5ed0cd5c52e72a302b1c28f7cfb6fd18
natansevero <natanseverolp@gmail.com>
2018-08-25 23:40:18 -0300

    starting the project and connecting to apollo

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import logo from './logo.svg';
+import './App.css';
+
+const client = new ApolloClient({
+  uri: 'https://api-useast.graphcms.com/v1/cjla7qgp402cp01cizmi1qybr/master'
+})
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
