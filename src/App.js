230a6c9f7d3603dd8d58fb8851bdb658b40809ad
natansevero <natanseverolp@gmail.com>
2018-08-25 23:54:15 -0300

    first graphql query

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
+import gql from 'graphql-tag'
+
 import logo from './logo.svg';
 import './App.css';
 
 const client = new ApolloClient({
   uri: 'https://api-useast.graphcms.com/v1/cjla7qgp402cp01cizmi1qybr/master'
 })
+
+const testQuery = gql`
+  {
+    posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+client
+  .query({
+    query: testQuery
+  })
+  .then(res => console.log(res))
 
 class App extends Component {
   render() {
